2c1d8e48a7d1382f86d93626d19078dddd27e693
Alex <sagaidak1337@gmail.com>
2020-05-16 18:02:32 +0300

    Updates

---
+++ precache-manifest.acf72292b89fb004d74fc0a03dc1720f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7442eb6a586f553bf3d53ce5c356c2d4",
+    "url": "/jokes/index.html"
+  },
+  {
+    "revision": "40b736344da75ce917ae",
+    "url": "/jokes/static/css/main.be3e1b99.chunk.css"
+  },
+  {
+    "revision": "248fb2527273fd77fc5e",
+    "url": "/jokes/static/js/2.008ae69d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jokes/static/js/2.008ae69d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40b736344da75ce917ae",
+    "url": "/jokes/static/js/main.91584e3f.chunk.js"
+  },
+  {
+    "revision": "4a92df3a29e60fd68e1e",
+    "url": "/jokes/static/js/runtime-main.3eeeacd2.js"
+  },
+  {
+    "revision": "dc12786ecb8d3f2069c390d0438dabca",
+    "url": "/jokes/static/media/chat.dc12786e.svg"
+  },
+  {
+    "revision": "b95d04cf1d4896e6e87c7aff8f090fd2",
+    "url": "/jokes/static/media/circle.b95d04cf.svg"
+  },
+  {
+    "revision": "da8245758a982d8eae61f9b37f3eb217",
+    "url": "/jokes/static/media/close-menu.da824575.svg"
+  },
+  {
+    "revision": "e977fb9e726c00e0765ae53cc9a0dd66",
+    "url": "/jokes/static/media/empty-heart.e977fb9e.svg"
+  },
+  {
+    "revision": "a71ae7514917391a2f9282c778f7c423",
+    "url": "/jokes/static/media/heart.a71ae751.svg"
+  },
+  {
+    "revision": "2cf3f9f85dd2d6fdaf13962ffc10f072",
+    "url": "/jokes/static/media/link.2cf3f9f8.svg"
+  },
+  {
+    "revision": "1c1c530453169ed8cf3e27b14b297c7f",
+    "url": "/jokes/static/media/menu.1c1c5304.svg"
+  }
+]);
